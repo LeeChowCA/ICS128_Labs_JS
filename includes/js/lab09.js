74ec8eac20ba7cb6de1bbf83325ffea0c2530038
JIANPING_ZHOU <zhoujianpingls@outlook.com>
2024-04-01 22:27:29 -0700

    almost finished

--- includes/js/lab09.js
+++ includes/js/lab09.js
@@ -29,7 +29,7 @@ loginButton.addEventListener("click", () => {
 })
 
 
-loginForm.addEventListener("submit", function(event ) {
+loginForm.addEventListener("submit", function (event) {
     event.preventDefault();
 
     if (firstNameInput.value.includes(" ") || firstNameInput.value === "") {
@@ -75,7 +75,7 @@ loginForm.addEventListener("submit", function(event ) {
     }
 
     if (firstNameVerify && secondNameVerify && ageVerify && emailVerify && phoneVerify && postalVerify) {
-        document.getElementById("loginForm").setAttribute("class","was-validated");
+        document.getElementById("loginForm").setAttribute("class", "was-validated");
         submitButton.setAttribute("data-bs-dismiss", "modal");
         let myModal = bootstrap.Modal.getInstance(loginModal);
         // get the modal named "loginModal"
@@ -100,8 +100,9 @@ logoutButton.addEventListener("click", () => {
     loginButton.classList.remove("d-none");
     document.getElementById("welcomeMessage").classList.add("d-none");
 })
+//add eventListener for logout button
 
-let responseAPI = async() => {
+let responseAPI = async () => {
     let lat = 48.44490755561586;
     let lon = -123.38660990255518;
     let apiKey = "d11ec01c268e7bc2a984c522ae15807f";
@@ -111,7 +112,226 @@ let responseAPI = async() => {
     console.log(response);
     let data = await response.json();
     console.log(data);
+
+    $("#weatherDisplay").html(`${data.main.temp} C ${data.weather[0].main}`);
 }
+//use api to get the weather
 
 responseAPI();
+//call api to get the weather
+
+
+$(document).ready(() => {
+
+    $("#widgetDisplay").html(`<h2>Results</h2>`)
+
+    const roomPrice = {
+        standard: 300,
+        double: 500,
+        penthouse: 800
+    }
+    // roomPrice object for 3 types of rooms
+
+    let totalPrice = 0;
+    let totalDays = 0;
+    let canceledRoom = 0;
+    let totalRoom = 0;
+
+
+    let calculatePrice = () => {
+        let startDate = new Date($("#startDate").val());
+        let endDate = new Date($("#endDate").val());
+        let betweenDays = Math.round((endDate - startDate) / (1000 * 60 * 60 * 24));
+
+        let selectedRoomType = $(`input[name="roomType"]:checked`).val();
+        // target the input with name = roomType and get it's value
+        let pricePerNight = roomPrice[selectedRoomType];
+
+        if (betweenDays > 0) {
+            let totalPrice = betweenDays * pricePerNight;
+            $("#widgetDisplay").html(`
+            <p>Your length of stay is:${betweenDays}</p>
+            <p>$${pricePerNight} / night</p>
+            <p>Total: $${pricePerNight * betweenDays}</p>
+            `);
+            totalPrice += pricePerNight * betweenDays;
+            totalDays += betweenDays;
+            console.log(totalPrice);
+            console.log(totalDays);
+            totalRoom += 1;
+
+            $("#bookingDisplay").html(`
+                    <p>Room Booked - there are ${(totalRoom - canceledRoom)}/${totalRoom} Rooms Booked</p
+                    <p>Your length of stay is ${totalDays} days</p>
+                    <p>$${pricePerNight} / night</p>
+                    <p>Total: $${pricePerNight * betweenDays}</p>        
+                    `);
+        } else {
+            $("#widgetDisplay").html(`<p>Please choose valid dates</p>`);
+        }
+    }
+
+    let cancelRoom = () => {
+        canceledRoom += 1;
+        let selectedRoomType = $(`input[name="roomType"]:checked`).val();
+        // target the input with name = roomType and get it's value
+        let pricePerNight = roomPrice[selectedRoomType];
+
+        let startDate = new Date($("#startDate").val());
+        let endDate = new Date($("#endDate").val());
+        let betweenDays = Math.round((endDate - startDate) / (1000 * 60 * 60 * 24));
+
+        $("#bookingDisplay").html(`
+                    <p class="text-info">Room Booked - there are ${(totalRoom - canceledRoom)}/${totalRoom} Rooms Booked</p
+                    <p>Your length of stay is ${totalDays} days</p>
+                    <p>$${pricePerNight} / night</p>
+                    <p>Total: $${pricePerNight * betweenDays}</p> `);
+    }
+    // calculatePrice method, it will display the order info.
+
+    $("#bookRoom").on("click", calculatePrice);
+    //add click event listener to bookRoom button, when click the calculatePrice will be called
+
+    $("#cancelRoom").on("click", cancelRoom)
+})
+
+
+
+const arrHotelRooms = [
+    {
+        name: "Standard",
+        description:"Single room with a bed",
+        img:"includes/imgs/room1.jpg",
+        price: "$150"
+    },{
+        name:"Double",
+        description: "Double Room with a bed",
+        img:'includes/imgs/room2.jpg',
+        price: "$120"
+    },{
+        name:"Penthouse",
+        description: "King Size Bed<br>Bar<br>Jacuzzi",
+        img:'includes/imgs/room3.jpg',
+        price: "$200"
+    }, ];
+
+let hotelRoomsDisplay = document.querySelector("#hotelRoomsDisplay");
+
+
+function hotelDisplay() {
+    for (let i = 0; i < arrHotelRooms.length; i++){
+        const card = document.createElement("div");
+        const row = document.createElement("div");
+        const divImg = document.createElement("div");
+        const hotelImg = document.createElement("img");
+        const divForRightSide = document.createElement("div");
+        const cardBody = document.createElement("div");
+        const cardHeader = document.createElement("div");
+        const cardText = document.createElement("p");
+        const cardPrice = document.createElement("p");
+        const divButton = document.createElement("div");
+        const cardButton1 = document.createElement("button");
+        const cardButton2 = document.createElement("button");
+        const cardButton3 = document.createElement("button");
+
+
+        card.setAttribute("class", "card mb-1");
+        row.setAttribute("class", "row g-0");
+        divImg.setAttribute("class", "col-5");
+        hotelImg.setAttribute("src", arrHotelRooms[i].img);
+        hotelImg.setAttribute("class", "img-fluid rounded-start");
+        divForRightSide.setAttribute("class", "col-7");
+        cardBody.setAttribute("class", "card-body");
+        cardHeader.setAttribute("class", "card-header");
+        cardText.setAttribute("class", "card-text");
+        cardPrice.setAttribute("class", "card-text");
+        divButton.setAttribute("class", "d-md-flex justify-content-md-end")
+        if (i === 0) {
+            cardButton1.setAttribute("class", "btn btn-primary");
+            cardButton1.setAttribute("type", "button ");
+        } else if (i === 1) {
+            cardButton2.setAttribute("class", "btn btn-primary");
+            cardButton2.setAttribute("type", "button ");
+        } else {
+            cardButton3.setAttribute("class", "btn btn-primary");
+            cardButton3.setAttribute("type", "button ");
+        }
+
+
+        hotelRoomsDisplay.appendChild(card);
+        card.appendChild(row);
+        row.appendChild(divImg);
+        divImg.appendChild(hotelImg);
+        row.appendChild(divForRightSide);
+        divForRightSide.appendChild(cardHeader);
+        divForRightSide.appendChild(cardBody);
+        cardBody.appendChild(cardText);
+        cardBody.appendChild(cardPrice);
+        cardBody.appendChild(divButton);
+
+        if (i === 0) {
+            divButton.appendChild(cardButton1);
+        } else if (i === 1) {
+            divButton.appendChild(cardButton2);
+        } else
+            divButton.appendChild(cardButton3);
+
+
+
+        cardHeader.innerHTML = arrHotelRooms[i].name;
+        cardText.innerHTML = arrHotelRooms[i].description;
+        cardPrice.innerHTML = arrHotelRooms[i].price;
+        cardButton1.innerHTML = "Book Room";
+        cardButton2.innerHTML = "Book Room";
+        cardButton3.innerHTML = "Book Room";
+
+        cardButton1.addEventListener("click", () => {
+            alert("your room is $150 per night");
+        })
+
+        cardButton2.addEventListener("click", () => {
+            alert("your room is $120 per night");
+        })
+
+        cardButton3.addEventListener("click", () => {
+            alert("your room is $200 per night");
+        })
+
+
+
+    }
+}
+hotelDisplay();
+
+const part0201 = document.getElementById("part0201");
+const sampleTable = document.getElementById("sampleTable");
+const myButton = document.getElementById("myButton");
+let counter = 1;
+
+myButton.addEventListener("click", () => {
+    counter++;
+
+    const table = document.createElement("table");
+    const tr = document.createElement("tr");
+    const td1 = document.createElement("td");
+    const td2 = document.createElement("td");
+    const tbody = document.createElement("tbody");
+
+    table.setAttribute("class", "table-striped table table-dark mb-0");
+    table.setAttribute("border", "1");
+
+    td1.innerHTML = `Row${counter} cell1`;
+    td2.innerHTML = `Row${counter} cell2`;
+
+    part0201.appendChild(table);
+
+    table.appendChild(tbody)
+    tbody.appendChild(tr);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+})
+
+
+
+
 
